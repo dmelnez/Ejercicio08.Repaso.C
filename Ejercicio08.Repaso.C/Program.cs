80c549b3de397e2158440097f279fc747a49064a
DESKTOP-JV3V0TT\dmelnez <csi23-dmelnez@colegioaltair.net>
2024-03-13 11:16:42 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio08.Repaso.C/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Ejercicio08.Repaso.C
+{
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("INTRODUCE UNA FRASE: ");
+            string frasePrimeraUsu = Console.ReadLine();
+
+            Console.WriteLine(frasePrimeraUsu);
+
+
+            Console.WriteLine("INTRODUZCA UN CARACTER A REEMPLAZAR DE LA FRASE");
+            char caracterPrimeroAReemplazar = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("CARACTER A REEMPLAZAR");
+            char caracterReemplazado = Convert.ToChar(Console.ReadLine());
+
+
+            string fraseModificada =  frasePrimeraUsu.Replace(caracterPrimeroAReemplazar, caracterReemplazado);
+
+            Console.WriteLine(fraseModificada);
+
+       
+
+           
+
+
+
+
+
+        }
+
+    }
+
+
+}
+
+
